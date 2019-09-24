152820c574fb470ce2897dddfba33b1530bd045f
Tegar Lucky'Q Oakley <55655976+tegaroakley@users.noreply.github.com>
2019-09-24 16:01:00 +0900

    Add files via upload

---
+++ src/pbo2/pkg10118066/latihan12/aritmatika/PBO210118066Latihan12Aritmatika.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118066.latihan12.aritmatika;
+
+/**
+ *
+ * @author 
+ * NAMA  : Tegar Lucky'Q Oakley 
+ * NIM   : 10118066 
+ * KELAS : IF2 
+ * Deskripsi Program : Program ini berisi program untuk Menunjukkan sistem perhitungan aritmatika
+ */
+public class PBO210118066Latihan12Aritmatika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 25;
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("b / a = " + (b / a));
+        System.out.println("b % a = " + (b % a));
+        System.out.println("c % a = " + (c % a));
+        System.out.println("a++   = " + (a++));
+        System.out.println("b--   = " + (a--));
+        System.out.println("d++   = " + (d++));
+        System.out.println("++d   = " + (++d));
+        
+    }
+    
+}
